874962fd47f7f442c22616b907d897c779ebf006
Juan Cucho <juan.cucho@outlook.com>
2017-09-29 22:00:17 -0500

    Agregar archivos de proyecto.

---
+++ Cibertec.Repositories.Dapper/Northwind/NorthwindUnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Cibertec.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Cibertec.Repositories.Northwind;
+
+namespace Cibertec.Repositories.Dapper.Northwind
+{
+    public class NorthwindUnitOfWork : IUnitOfWork
+
+    {
+        public NorthwindUnitOfWork(string connectioString)
+        {
+            Customers = new CustomerRepository(connectioString);
+            Orders = new OrderRepository(connectioString);
+            /*
+            OrderItems = new CustomerRepository(connectioString);
+            Products = new CustomerRepository(connectioString);
+            Suppliers = new CustomerRepository(connectioString);
+            Users = new CustomerRepository(connectioString);
+            */
+        }
+        public ICustomerRepository Customers { get; private set; }
+
+        public IOrderRepository Orders { get; private set; }
+
+        public IOrderItemRepository OrderItems { get; private set; }
+
+        public IProductRepository Products { get; private set; }
+
+        public ISupplierRepository Suppliers { get; private set; }
+
+        public IUserRepository Users { get; private set; }
+    }
+}
---
+++ Cibertec.Repositories.Dapper/Northwind/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Cibertec.Models;
+using Cibertec.Repositories.Northwind;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Cibertec.Repositories.Dapper.Northwind
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(string connectionString) : base(connectionString)
+        {
+        }       
+    }
+}
