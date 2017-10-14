b169db219f18ac18dcbcb6a065316c4d00f20d6c
Juan Cucho <juan.cucho@outlook.com>
2017-10-13 19:52:36 -0500

    Se agrego proyecto de prueba Test para WebAP

---
+++ Cibertec.WebApi.Tests/ConfigSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cibertec.WebApi.Tests
+{
+    public static class ConfigSettings
+    {
+        public static string NorthWindConnectionString = "Server=.;Database=Northwind_Lite; Trusted_Connection=True;MultipleActiveResultSets=True";
+    }
+}
